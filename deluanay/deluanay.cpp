89a153ede6bca92f863154f3e10b017db76cde97
croko22 <kvchambit@gmail.com>
2024-07-20 10:24:26 -0500

    feat: Add Delaunay triangulation code

---
+++ deluanay/deluanay.cpp
@@ -0,0 +1,38 @@
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_3.h>
+#include <CGAL/point_generators_3.h>
+#include <CGAL/draw_triangulation_3.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Delaunay_triangulation_3<K> DT3;
+typedef CGAL::Creator_uniform_3<double, K::Point_3> Creator;
+int main()
+{
+    std::vector<K::Point_3> points;
+
+    std::ifstream file("bunny.pcd");
+    std::string line;
+    double x, y, z;
+    while (getline(file, line))
+    {
+        std::istringstream iss(line);
+        if (iss >> x >> y >> z)
+            points.push_back(K::Point_3(x, y, z));
+    }
+
+    // for (auto p : points)
+    // {
+    //     std::cout << p << std::endl;
+    // }
+
+    DT3 dt3(points.begin(), points.end());
+    CGAL::draw(dt3);
+
+    std::ofstream out("bunny.off");
+    out << dt3;
+
+    return EXIT_SUCCESS;
+}
